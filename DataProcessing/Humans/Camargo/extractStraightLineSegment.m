8ba0a4703500de2b492624d0d6ffb4af9109cf18
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2025-09-15 09:43:11 -0400

    Update extractStraightLineSegment.m

--- DataProcessing/Humans/Camargo/extractStraightLineSegment.m
+++ DataProcessing/Humans/Camargo/extractStraightLineSegment.m
@@ -1,13 +1,20 @@
 function bool_vector = extractStraightLineSegments(angle_vector)
-% Extract the times at which the subject is in one of the two long straight
-% lines segments of the overground ... 
+% EXTRACTSTRAIGHTLINESEGMENTS : Identifies the straight line locomotion segments and returns the corresponding time masks
+% 
+% INPUTS:
+%    angle_vector : Movement direction vector
+%
+% OUTPUTS:
+%    bool_vector : Masking vector identifiying the straight line locomotion bouts
+% 
+% @Antoine De Comite - MIT 2025
 
-% Transform the angle in degrees 
+% Transforming the original angles (rad) in degrees 
 angle_deg = 360*angle_vector / (2*pi);
 angle_deg = abs(angle_deg);
 
+% Indentifying the time points where the orientation is around the straight line
 bool_angle = ((angle_deg<110) & (angle_deg>70));
-% Let's search for long segments of 1 in that vector 
 begins = zeros(1,1); lengths = zeros(1,1);
 for ii = 2:length(bool_angle)
     if((bool_angle(ii)==1) && (bool_angle(ii-1)==0))
