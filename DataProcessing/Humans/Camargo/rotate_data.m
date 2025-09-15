372c11ffb6d3e49e75132099b0640d5a92ab1b2f
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2025-09-15 09:40:00 -0400

    Update rotate_data.m

--- DataProcessing/Humans/Camargo/rotate_data.m
+++ DataProcessing/Humans/Camargo/rotate_data.m
@@ -1,6 +1,15 @@
 function data_output = rotate_data(data_input, angle, time_vector)
-
-
+% DATA_OUTPUT : Rotates the data to align the data points along the main movement direction
+%
+% INPUTS:
+%  data_input : Data to be rotated
+%  angle : Rotation angle to be applied
+%  time_vector : Mask to extract the relevant data 
+%
+% OUTPUTS:
+%  data_output : Rotated data
+%
+% @Antoine De Comite - MIT 2025
 tmp_data =  data_input(time_vector,:,:);
 data_output = tmp_data;
 data_output(:,:,1) = cos(angle) * tmp_data(:,:,1) + sin(angle) * tmp_data(:,:,3);
