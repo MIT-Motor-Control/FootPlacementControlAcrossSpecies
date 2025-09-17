0f8fd78f1d7b615605ea6a75a89cbddf006d2cbc
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2025-09-17 14:16:35 -0400

    Update rescale_data_to_mm.m

--- DataProcessing/Flies/rescale_data_to_mm.m
+++ DataProcessing/Flies/rescale_data_to_mm.m
@@ -1,5 +1,14 @@
 function [output_data] = rescale_data_to_mm(input_data)
-% Rescale the data from abstract units of size to mm 
+%RESCALE_DATA_TO_MM transforms the data from pixels to mm
+% 
+% INPUTS 
+% - input_data: contains the input unnormalized data
+%
+% OUTPUTS
+% - output_data: contains the output normalized data
+%
+% @Antoine De Comite - MIT 2025
+
 scaling_factor_x = 2.75/584.7894;
 scaling_factor_y = 2.2/458.1300;
 output_data = zeros(size(input_data));
@@ -15,7 +24,4 @@ for variable = 1 : size(input_data,2)
         output_data(:,variable) = input_data(:,variable);
     end
 end
-
-
-
 end
