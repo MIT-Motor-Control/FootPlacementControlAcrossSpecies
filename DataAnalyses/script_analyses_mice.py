f02d1782cfd7b952e2c00001aff543e284a6bd9e
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2025-09-22 13:27:55 -0400

    update script mouse

--- DataAnalyses/script_analyses_mice.py
+++ DataAnalyses/script_analyses_mice.py
@@ -39,17 +39,9 @@ velocity_vector_od = get_velocity_vector(tot_input_list_od, tot_input_self_od)
 ###################################################
 ### PART 1 - Velocity-dependent ffwd controller ###
 ###################################################
-plot_feedforward_controller_final(velocity_vector, tot_output_list, tot_animal, tot_input_absolute, tot_output_absolute, tot_animal_absolute, bool_plot=True, bool_save=False, figname='feedforward_figures_mouse')
+plot_feedforward_controller_final(velocity_vector, tot_output_list, tot_animal, tot_input_absolute, tot_output_absolute, tot_animal_absolute, bool_plot=False, bool_save=False, figname='feedforward_figures_mouse')
 
 
-list_input_horizon, list_output_horizon = [], []
-list_input_horizon.append(tot_input_self), list_output_horizon.append(tot_output_self)
-for horizon in range(3,6):
-    input_list_horizon, output_list_horizon = load_data_horizon(horizon)
-    tot_input_self_horizon, tot_output_self_horizon = concatenate_data_horizon(input_list_horizon, output_list_horizon)
-    list_input_horizon.append(tot_input_self_horizon), list_output_horizon.append(tot_output_self_horizon)
-
-plot_correlation_horizon_final(list_input_horizon, tot_animal, bool_plot=True, bool_save=True, figname='horizon_correlation')
 
 
 ###############################################################
@@ -64,14 +56,6 @@ rsquare_front_foreaft_self = get_rsquare_matrix_self(tot_animal, tot_animal_od, 
 rsquare_front_lateral_self = get_rsquare_matrix_self(tot_animal, tot_animal_od, tot_input_list, tot_input_self, tot_input_list_od, tot_input_self_od, tot_output_list, tot_output_self_od, bool_hind=0, bool_lat=1)
 rsquare_hind_foreaft_self = get_rsquare_matrix_self(tot_animal, tot_animal_od, tot_input_list, tot_input_self, tot_input_list_od, tot_input_self_od, tot_output_list, tot_output_self_od, bool_hind=1, bool_lat=0)
 rsquare_hind_lateral_self = get_rsquare_matrix_self(tot_animal, tot_animal_od, tot_input_list, tot_input_self, tot_input_list_od, tot_input_self_od, tot_output_list, tot_output_self_od, bool_hind=1, bool_lat=1)
-# Get the rsquares matrix for the timing 
-rsquare_front_time_body = get_rsquare_matrix_feedback_time(tot_animal, tot_input_list, tot_output_absolute, bool_hind=0)
-rsquare_hind_time_body = get_rsquare_matrix_feedback_time(tot_animal, tot_input_list, tot_output_absolute, bool_hind=1)
-rsquare_front_time_self = get_rsquare_matrix_self_time(tot_animal, tot_input_list, tot_input_self, tot_output_absolute, bool_hind=0)
-rsquare_hind_time_self = get_rsquare_matrix_self_time(tot_animal, tot_input_list, tot_input_self, tot_output_absolute, bool_hind=1)
-
-plot_rsquares_time(rsquare_front_time_body, rsquare_hind_time_body, rsquare_front_time_self, rsquare_hind_time_self, bool_plot=False, bool_save=False, figname='rsquares_contact_timing')
-
 
 
 
@@ -81,6 +65,14 @@ for animal in range(ind_difference.shape[0]):
     ind_difference[animal,1] = np.max(rsquare_front_lateral_body[animal,:] - rsquare_front_lateral_self[animal,:])
     ind_difference[animal,2] = np.max(rsquare_hind_foreaft_body[animal,:] - rsquare_hind_foreaft_self[animal,:])
     ind_difference[animal,3] = np.max(rsquare_hind_lateral_body[animal,:] - rsquare_hind_lateral_self[animal,:])
+
+fig, axs = plt.subplots(1,1,figsize=(3,3))
+axs.spines[['top','right']].set_visible(False)
+axs.scatter(np.linspace(1.8,2.2,80),ind_difference[:,1],color='r',s=5, alpha=0.5)
+axs.scatter(2, np.nanmedian(ind_difference[:,1]), color='r',s=20)
+axs.plot([2,2], [np.nanmedian(ind_difference[:,1])+scipy.stats.iqr(ind_difference[:,1], nan_policy='omit'),np.nanmedian(ind_difference[:,1])-scipy.stats.iqr(ind_difference[:,1], nan_policy='omit')],'r',lw=2)
+axs.set_xlim([0.5,3.5]), axs.set_ylabel('Control Magnitude'), axs.set_ylim([0,1])
+plt.tight_layout()
 
 
 plot_rsquares_final(rsquare_front_foreaft_body, rsquare_front_lateral_body, rsquare_hind_foreaft_body, rsquare_hind_lateral_body,
@@ -102,12 +94,10 @@ for horizon in range(3,6):
 
 plot_correlation_horizon_final(list_input_horizon, tot_animal, bool_plot=False, bool_save=False, figname='horizon_correlation')
 
-# 3.3 Normalized feedback gains for the lateral deviation
-regression_matrix = compute_regression_fr(tot_animal, tot_input_list, tot_output_list, vel_tier,0)
-plot_normalized_feedback_gains(regression_matrix, bool_plot=False, bool_save=False, figname='fbgains_lat_lat')
+
 
 ###############################################################################
 ### PART 4 - Investigation of the laterality for the foot placement control ###
 ###############################################################################
 regression_small, regression_large = regression_fr_laterality_velidp(tot_animal, tot_input_list, tot_output_list, 0)
-plot_laterality_regression(regression_small, regression_large, bool_plot=False, bool_save=False, figname='laterality')
+plot_laterality_regression(regression_small, regression_large, bool_plot=True, bool_save=False, figname='laterality')
