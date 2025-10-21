58fad264690a410c6fb30d558f2e611073809827
decomiteA <antoinedecomite@gmail.com>
2025-10-21 17:29:29 -0400

    comment script analyses flies

--- DataAnalyses/script_analyses_flies.py
+++ DataAnalyses/script_analyses_flies.py
@@ -1,3 +1,4 @@
+# This script investigates the feedforward and feedback components of the fly locomotion datasets presented in the paper.
 import os, sys
 import pickle
 import numpy as np 
@@ -8,17 +9,28 @@ from utils.utils_flies import *
 from utils.plot_functions_flies import *
 warnings.filterwarnings('ignore')
 
+
+
 # Loading and pre-processing the data 
 body_io, self_io = load_all_data_mm()
 video_list, video_list_od = load_video_data_mm()
-
+# Extracting the input and output data for the body-state and baseline foot placement control models
 tot_input_list, tot_input_self, tot_output_list, tot_output_self = concatenate_data(body_io[0], body_io[1], self_io[0], self_io[1])
 tot_input_list, tot_input_self, tot_output_list, tot_output_self = flip_data(tot_input_list, tot_input_self, tot_output_list, tot_output_self)
 
 
+###################################################
+### PART 1 - Velocity dependent ffwd controller ###
+###################################################
+
 plot_feedforward_controller_final(tot_input_list, tot_output_list, video_list, bool_plot=False, bool_save=False, figname='feedforward_outputs_fly')
 
-# Body prediction
+
+########################################################
+### PART 2 - Body-state depedent feedback controller ###
+########################################################
+
+# Computing the rsquares matrices for the body prediction
 rsquare_leg1_front,_ = get_rsquare_matrix_normalization(tot_input_list, tot_output_list, video_list, 0, 0)
 rsquare_leg1_lat,_ = get_rsquare_matrix_normalization(tot_input_list, tot_output_list, video_list, 0, 1)
 
@@ -28,7 +40,7 @@ rsquare_leg2_lat,fb_gains = get_rsquare_matrix_normalization(tot_input_list, tot
 rsquare_leg3_front,_ = get_rsquare_matrix_normalization(tot_input_list, tot_output_list, video_list, 2, 0)
 rsquare_leg3_lat,_ = get_rsquare_matrix_normalization(tot_input_list, tot_output_list, video_list, 2, 1)
 
-# Self prediction
+# Computing the rsquares matrices for the self prediction
 rsquare_leg1_front_self = get_rsquare_matrix(tot_input_self, tot_output_list, video_list, 0, 0)
 rsquare_leg1_lat_self = get_rsquare_matrix(tot_input_self, tot_output_list, video_list, 0, 1)
 
@@ -41,6 +53,14 @@ rsquare_leg3_lat_self = get_rsquare_matrix(tot_input_self, tot_output_list, vide
 list_body = [rsquare_leg1_front, rsquare_leg1_lat, rsquare_leg2_front, rsquare_leg2_lat, rsquare_leg3_front, rsquare_leg3_lat]
 list_self = [rsquare_leg1_front_self, rsquare_leg1_lat_self, rsquare_leg2_front_self, rsquare_leg2_lat_self, rsquare_leg3_front_self, rsquare_leg3_lat_self]
 
+plot_rsquares_final(list_body, list_self, bool_plot=False, bool_save=False, figname='final_rsquares_flies_video')
+
+
+############################################################
+### PART 3 - Characterization of the feedback controller ###
+############################################################
+
+# Computing the difference between the body state and baseline models.
 
 ind_difference = np.zeros((rsquare_leg1_front.shape[0],6))
 for ii in range(ind_difference.shape[0]):
@@ -51,11 +71,9 @@ for ii in range(ind_difference.shape[0]):
     ind_difference[ii,4] = np.max(rsquare_leg3_front[ii,:]-rsquare_leg3_front_self[ii,:])
     ind_difference[ii,5] = np.max(rsquare_leg3_lat[ii,:]-rsquare_leg3_lat_self[ii,:])
 
-
-plot_rsquares_final(list_body, list_self, bool_plot=False, bool_save=False, figname='final_rsquares_flies_video')
-
-
+# Representation of the control magnitude
 fig, axs = plt.subplots(1,1,figsize=(3,3))
+fig.suptitle('Control magnitude', fontweight='bold')
 axs.spines[['top','right']].set_visible(False)
 axs.scatter(np.linspace(2.8,3.2,8),ind_difference[:,1],color='r',s=5, alpha=0.5)
 axs.scatter(3, np.nanmedian(ind_difference[:,1]), color='r',s=20)
@@ -63,7 +81,7 @@ axs.plot([3,3], [np.nanmedian(ind_difference[:,1])+scipy.stats.iqr(ind_differenc
 axs.set_xlim([0.5,3.5]), axs.set_ylabel('Control Magnitude'), axs.set_ylim([0,1])
 plt.tight_layout()
 
-
+# Control timescale timescale analyses 
 list_self_with_horizons = [] 
 list_input_horizon, list_output_horizon = [], []
 list_self_with_horizons.append(tot_input_self)
@@ -79,7 +97,9 @@ for horizon in tqdm(range(2,6)):
 plot_successive_contacts_fly(list_self_with_horizons, video_list, bool_plot=False, bool_save=False, figname='successive_contacts')
 
 
-
+###########################################################################
+### PART 4 - Investigation of the laterality for foot placement control ###
+###########################################################################
 
 
 regression_small, regression_large = regression_fr_laterality_velidp(tot_input_list, tot_output_list, video_list, 1)
